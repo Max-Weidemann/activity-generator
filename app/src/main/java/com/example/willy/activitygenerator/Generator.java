bad0857bd3842589c1c4c25963f78c90542b08b0
Willy <willyatomgurke@freenet.de>
2018-02-10 22:29:05 +0100

    Textsize increased and Toolbar added.

--- app/src/main/java/com/example/willy/activitygenerator/Generator.java
+++ app/src/main/java/com/example/willy/activitygenerator/Generator.java
@@ -3,9 +3,14 @@ package com.example.willy.activitygenerator;
 import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -18,6 +23,9 @@ public class Generator extends AppCompatActivity {
     // Buttons
     Button btnGenerate;
 
+    //Toolbar
+    Toolbar mToolbar;
+
     // Text Views
     TextView txtWord;
 
@@ -26,6 +34,10 @@ public class Generator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generator);
+
+        //Activate Toolbar
+        mToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
 
         // Linking
         btnGenerate = findViewById(R.id.button_generate);
@@ -40,6 +52,17 @@ public class Generator extends AppCompatActivity {
                 txtWord.setText(word);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     private ArrayList<String> loadDictionary(){
